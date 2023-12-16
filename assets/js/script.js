483e590b524ed95b092bd42a932653ca73b89d90
MarcoGotti <mrc.gotti@gmail.com>
2023-12-16 01:08:32 +0000

    add milestone 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 /* 
 
-Milestone 3
-Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
 BONUS
 1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 2 - popolare le options della select della milestone 3 dinamicamente.
@@ -143,12 +140,22 @@ Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno sti
  */
 
 const rowEl = document.querySelector('.row');
+const iconType = document.getElementById('selectIconType').value
 
-for (let i = 0; i < list.length; i++) {
-    const iconName = list[i].name;
-    const iconPrefix = list[i].prefix;
+const typeSelect = list.filter((element) =>{
+    if (element.type === iconType) {
+        return true;        
+    } else if (iconType === 'all') {
+        return true;
+    }   
+});
+console.log(typeSelect);
+
+for (let i = 0; i < typeSelect.length; i++) {
+    const iconName = typeSelect[i].name;
+    const iconPrefix = typeSelect[i].prefix;
     //Milestone 2 colora le icone con valore color:
-    const iconColor = list[i].color;
+    const iconColor = typeSelect[i].color;
     /* ******************************* */
     //const iconEl = generateIcolnEl(iconName, iconPrefix)
     /* ******************************* */
@@ -180,3 +187,6 @@ function generateColMarkUp(fontAwesomeIconClass, fontAwesomeIconName) {
     return colMarkUp
 }
 
+//Milestone 3 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+//manipola direttamente Array removing objects non utili con un array.filter. Sistema ciclo e function in modo da richiamarli con il nuovo Array 

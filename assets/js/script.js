036c36aeda300554288db3b4e821bddfe7a5037c
MarcoGotti <mrc.gotti@gmail.com>
2023-12-17 18:57:53 +0000

    head commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,4 @@
-/* 
 
-BONUS
-1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-2 - popolare le options della select della milestone 3 dinamicamente.
-
-Consigli del giorno
-Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
-
-NB: L'esercizio My Team con array di oggetti e questo sulle icone sono due esercizi chiave per poter svolgere la milestone che incombe. Dedicateci del tempo e approfondite gli argometi trattati.
-*/
-
-
-
-//  <i class="fa-solid fa-carrot"></i>
-//  <i class="fa-solid fa-apple-whole"></i>
-//  <i class="fa-solid fa-user-secret"></i>
 const list = [
 	{
 		name: 'cat',  
@@ -130,17 +114,10 @@ const list = [
 	}
 ];
 
-/* Milestone 1 Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
-
-Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina. Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi.
-
-Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
-
-Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
-*/
 
 const rowEl = document.querySelector('.row');
 const iconType = document.getElementById('selectIconType').value
+
 
 //create list according to selected property .type
 
@@ -185,16 +162,16 @@ for (let i = 0; i < typeSelectList.length; i++) {
     //Milestone 2 colora le icone con valore color:
     //const iconColor = typeSelectList[i].color;
 	//console.log(iconColor);
-/* ******************************************************* */
+/* ********************************************************************************** */
     //const iconEl = generateIcolnEl(iconName, iconPrefix)
-/* ******************************************************* */
+/* ********************************************************************************** */
     const iClass = `${iconPrefix}solid ${iconPrefix}${iconName}`;
 
     const colMarkUp = generateColMarkUp(iClass, iconName, randomColor);
     rowEl.insertAdjacentHTML("beforeend", colMarkUp);
 }
-/* ******************************************************* 
-At first trying to fit a - const iconEl = HTMLElement <i>...</i> - into TemplateLiteral ${iconEl} but not possible.
+/* ************************************************************************************** 
+At first trying to fit a // const iconEl = HTMLElement <i>...</i> // into TemplateLiteral ${iconEl} but not possible.
 
 function generateIcolnEl(name, prefix) {
     const iconEl = document.createElement('i');
@@ -202,7 +179,7 @@ function generateIcolnEl(name, prefix) {
     iconEl.className = iClass;   
     return iconEl
 } 
-*********************************************************** */
+**************************************************************************************** */
 
 function generateColMarkUp(fontAwesomeIconClass, fontAwesomeIconName, generatedRandomColor) {        
     const colMarkUp = `
@@ -216,11 +193,3 @@ function generateColMarkUp(fontAwesomeIconClass, fontAwesomeIconName, generatedR
     return colMarkUp
 }
 
-
-
-//Milestone 3 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
-//manipola direttamente Array removing objects non utili con un array.filter. Sistema ciclo e function in modo da richiamarli con il nuovo Array 
-
-// BONUS 1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F. 
-//2 - popolare le options della select della milestone 3 dinamicamente.
